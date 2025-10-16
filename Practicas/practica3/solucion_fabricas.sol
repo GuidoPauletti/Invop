89422c2b7ae6c7de578ade5148e253d79f35dd9a
Guido Pauletti <guido13pauletti@gmail.com>
2025-10-16 19:09:27 -0300

    up to date

---
+++ Practicas/practica3/solucion_fabricas.sol
@@ -0,0 +1,3 @@
+objective value:                            +infinity
+y1                                          +infinity 	(obj:38)
+y2                                          +infinity 	(obj:55)
